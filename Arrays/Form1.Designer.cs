01abd3b9c2d54df9e2b08eb75b78f59678c40cb5
julieagreen <julie.a.green@gmail.com>
2016-05-17 20:57:38 -0700

    Add project, created form, starting work on name entering

--- Arrays/Form1.Designer.cs
+++ Arrays/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Arrays
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblNames = new System.Windows.Forms.Label();
+            this.txtNames = new System.Windows.Forms.TextBox();
+            this.btnEnterName = new System.Windows.Forms.Button();
+            this.btnShowNames = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(91, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(219, 26);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(30, 36);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(48, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // lblNames
+            // 
+            this.lblNames.AutoSize = true;
+            this.lblNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNames.Location = new System.Drawing.Point(22, 79);
+            this.lblNames.Name = "lblNames";
+            this.lblNames.Size = new System.Drawing.Size(55, 16);
+            this.lblNames.TabIndex = 3;
+            this.lblNames.Text = "Names:";
+            // 
+            // txtNames
+            // 
+            this.txtNames.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtNames.Enabled = false;
+            this.txtNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNames.Location = new System.Drawing.Point(91, 79);
+            this.txtNames.Multiline = true;
+            this.txtNames.Name = "txtNames";
+            this.txtNames.ReadOnly = true;
+            this.txtNames.Size = new System.Drawing.Size(219, 323);
+            this.txtNames.TabIndex = 4;
+            this.txtNames.TabStop = false;
+            // 
+            // btnEnterName
+            // 
+            this.btnEnterName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnterName.Location = new System.Drawing.Point(345, 29);
+            this.btnEnterName.Name = "btnEnterName";
+            this.btnEnterName.Size = new System.Drawing.Size(108, 37);
+            this.btnEnterName.TabIndex = 5;
+            this.btnEnterName.Text = "Enter Name";
+            this.btnEnterName.UseVisualStyleBackColor = true;
+            this.btnEnterName.Click += new System.EventHandler(this.btnEnterName_Click);
+            // 
+            // btnShowNames
+            // 
+            this.btnShowNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowNames.Location = new System.Drawing.Point(345, 90);
+            this.btnShowNames.Name = "btnShowNames";
+            this.btnShowNames.Size = new System.Drawing.Size(108, 37);
+            this.btnShowNames.TabIndex = 6;
+            this.btnShowNames.Text = "Show Names";
+            this.btnShowNames.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(486, 425);
+            this.Controls.Add(this.btnShowNames);
+            this.Controls.Add(this.btnEnterName);
+            this.Controls.Add(this.txtNames);
+            this.Controls.Add(this.lblNames);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmMain";
+            this.Text = "Arrays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblNames;
+        private System.Windows.Forms.TextBox txtNames;
+        private System.Windows.Forms.Button btnEnterName;
+        private System.Windows.Forms.Button btnShowNames;
     }
 }
 
--- Arrays/Form1.cs
+++ Arrays/Form1.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace Arrays
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btnEnterName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
